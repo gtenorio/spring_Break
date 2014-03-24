79946ac2d1b680df6b1ca9a54c931ee7b5cda681
rachan5 <raymondchan9999@hotmail.com>
2014-03-23 17:32:32 -0700

    testing

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+  for (int i = 0; i < 8; i++)
+  {
+    cout << " Does it work okay?" << endl;
+  }
+}
+
