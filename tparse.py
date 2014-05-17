e888dd46cbf07171adcd9a407fa1d8e01546f692
Giovanni Tenorio <giotenorio868@gmail.com>
2014-05-16 23:56:37 -0700

    PLZ

--- tparse.py
+++ tparse.py
@@ -1,25 +1,46 @@
 
 from HTMLParser import HTMLParser
+import urllib2
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
-  def handle_starttag(self, tag, attrs):
-    print "Encountered a start tag:", tag
-  def handle_endtag(self, tag):
-    print "Encountered an end tag :", tag
-  def handle_data(self, data):
-    print "Encountered some data  :", data
+    #def handle_starttag(self, tag, attrs):
+    #    print "Encountered a start tag:", tag
+    #def handle_endtag(self, tag):
+    #    print "Encountered an end tag :", tag
+    def handle_data(self, data):
+        if data == "\n" or data == "" or data == " " or data == "\t" or data == "\x0b" or data == "\r" or data == "\x0c":
+            pass
+        else:
+            print data
 
 # instantiate the parser and fed it some HTML
-f = open("Bulbasaur.html")
+
+#f = open("Bulbasaur.html")
 
 parser = MyHTMLParser()
 #parser.feed('<html><head><title>Test</title></head>'
 #            '<body><h1>Parse me!</h1></body></html>')
-flag = 0
-for line in f:
-  #print line[9:22]
-  if (line[9:22] == "Begin Content"):
-    flag = 1
-  if (flag == 1):
-    parser.feed(line)
+
+for i in range(1, 5):
+    html_str = "http://www.psypokes.com/dex/psydex/"
+    if i < 10:
+        html_str += ("00" + str(i))
+    elif 10 < i < 100:
+        html_str += ("0" + str(i))
+    else:
+        html_str += (str(i))
+
+    html_str += "/general"
+
+    f = urllib2.urlopen(html_str)
+
+    flag = 0
+    for line in f:
+        #print line[9:22]
+        if line[9:22] == "Begin Content":
+            flag = 1
+        if flag == 1:
+            parser.feed(line)
+
+    html_str = ""
